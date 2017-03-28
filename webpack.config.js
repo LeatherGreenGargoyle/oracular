aaa7eb38d81d453729acf8ad1fabae72381e4a45
Andrew Tran <avtran23@gmail.com>
2017-03-27 19:00:42 -0700

    [setup/fix]Add HtmlWebpackPlugin, start script

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
 const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
 module.exports = {
   entry: './client/index.js',
   output: {
@@ -8,7 +16,8 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
-      { test: /.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
     ]
-  }
+  },
+  plugins: [HtmlWebpackPlugin]
 }
